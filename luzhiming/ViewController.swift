600105c246cf231597118b2c2f6be1290d71bda2
QuanchaoSong <strawberry1234561@gmail.com>
2025-11-03 22:37:26 +0800

    Initial Commit

---
+++ luzhiming/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  luzhiming
+//
+//  Created by Albus on 2025/11/3.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
